516640c4c5bd9283cf62289c2314ed436a1c6f22
andlrutt <71574118+andlrutt@users.noreply.github.com>
2023-05-04 09:08:38 -0400

    nice

--- js/raas.js
+++ js/raas.js
@@ -1,16 +1,40 @@
 var g_scene_graph = null;
-const RAAS_LOCATION = "http://3.145.40.172";
+var initial_scene_graph = null
+const RAAS_LOCATION = "http://52.15.147.74";
 var editor = null;
 var movieFrames = [];
+var recording = false
 
 async function initial_render() {
 	let res = await fetch(`${RAAS_LOCATION}/render/`);
 	document.querySelector(".render").src = URL.createObjectURL(await res.blob());
 	g_scene_graph = await get_scene_graph();
+	initial_scene_graph = JSON.parse(JSON.stringify(g_scene_graph))
 }
 
 async function btnClick() {
+	let renderButton = document.getElementById('rerender_button')
+	renderButton.disabled = true
+	renderButton.textContent = "Rendering..."
 	await re_render(g_scene_graph);
+	renderButton.textContent = "Re-render"
+	renderButton.disabled = false
+}
+
+function toggle_recording() {
+	let record_button = document.getElementById("record_button")
+	recording = !recording
+	console.log(`now ${!recording ? 'not ' : ''}recording`)
+	record_button.textContent = recording ? "Stop Recording" : "Start Recording"
+}
+
+async function render_initial_scene_graph() {
+	let renderButton = document.getElementById('rerender_button')
+	renderButton.disabled = true
+	renderButton.textContent = "Rendering..."
+	await re_render(initial_scene_graph)
+	renderButton.textContent = "Re-render"
+	renderButton.disabled = false
 }
 
 async function get_scene_graph() {
@@ -20,7 +44,7 @@ async function get_scene_graph() {
 	// To modify the camera, you should use these vectors rather than the transformation vectors.
 	scene_graph.camera.position = [0.0, 0.0, 1.0];
 	scene_graph.camera.up = [0.0, 1.0, 0.0];
-	scene_graph.camera.view = [-0.0, 0.0, -1.0];
+	scene_graph.camera.view = [0.0, 0.0, -1.0];
 
 	scene_graph.resolution = "1920x1080"; // This can be a description of the resolution such as 720p, 4K, 8K, etc, or a width by height such as 1920x1080.
 	scene_graph.samples_per_pixel = 1; // The number of samples per pixel to use when rendering.
@@ -37,7 +61,7 @@ async function re_render(scene_graph) {
 
 	const res = await fetch(`${RAAS_LOCATION}/render/`, options);
 	document.querySelector(".render").src = URL.createObjectURL(await res.blob());
-	console.log("Scene Re-rendered");
+	console.log("Scene Re-rendered", scene_graph);
 }
 
 async function render_movie(key_frames) {
@@ -45,7 +69,7 @@ async function render_movie(key_frames) {
 		body: JSON.stringify({
 			fps: 10,
 			frames: key_frames,
-			length: 5,
+			length: 10,
 		}),
 		headers: { "Content-Type": "application/json" },
 		method: "POST",
@@ -84,14 +108,37 @@ async function initialize_json_editor() {
 	const updatedJson = editor.get();
 }
 
-function add_movie_keyframe() {
+function add_movie_keyframe(transformText) {
 	movieFrames.push(JSON.parse(JSON.stringify(g_scene_graph)));
+	create_keyframe_button(transformText)
+}
+
+function create_keyframe_button(transformText) {
+	let parent = document.getElementById("keyframes")
+	let keyframeButton = document.createElement("button")
+	keyframeButton.id = movieFrames.length - 1
+	if (recording) {
+		keyframeButton.textContent = transformText
+	}
+	else {
+		keyframeButton.textContent = `Keyframe ${movieFrames.length - 1}`
+	}
+	keyframeButton.onclick = () => {
+		g_scene_graph = JSON.parse(JSON.stringify(movieFrames[parseInt(keyframeButton.id)]))
+		re_render(g_scene_graph)
+	}
+	parent.appendChild(keyframeButton)
 }
 
 async function generate_movie() {
 	console.log("Generating movie...")
 	const startTime = new Date().getTime()
+	let movieButton = document.getElementById('generate_movie_button')
+	movieButton.textContent = "Generating movie..."
+	movieButton.disabled = true
 	await render_movie(movieFrames);
+	movieButton.textContent = "Generate Movie"
+	movieButton.disabled = false
 	console.log(`Generated and downloaded movie. Took ${(new Date().getTime() - startTime) / 1000} seconds.`)
 }
 
@@ -119,6 +166,10 @@ function change_rotation(xDegrees, yDegrees) {
 	// applies rotations to camera
 	glMatrix.vec3.transformMat4(g_scene_graph.camera.position, g_scene_graph.camera.position, rotationMatrix);
 
+	if (recording) {
+		add_movie_keyframe(`Camera rotate: ${xDegrees}° in x, ${yDegrees}° in y`)
+	}
+
 	return g_scene_graph
 }
 
@@ -138,8 +189,25 @@ function change_zoom(delta) {
 		g_scene_graph.camera.position[i] = g_scene_graph.camera.view[i] + relativeCameraPosition[i];
 	}
 
+	if (recording) {
+		add_movie_keyframe(`Camera zoom ${delta > 0 ? 'out' : 'in'} of ${delta} units`)
+	}
+
 	return g_scene_graph;
 }
+
+
+
+function turn_camera(direction, amount, directionAsString) {
+	for (let i = 0; i < 3; i++) {
+		g_scene_graph.camera.view[i] += direction[i] * amount * 0.25
+	}
+	if (recording) {
+		add_movie_keyframe(`Camera turn ${amount} units ${directionAsString}`)
+	}
+	console.log(g_scene_graph.camera.view)
+}
+
 
 function normalize(vec) {
 	const length = Math.sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]);
@@ -150,6 +218,7 @@ function normalize(vec) {
 
 	return vec;
 }
+
 
 function degrees_to_radians(degrees) {
 	return degrees * Math.PI / 180;
